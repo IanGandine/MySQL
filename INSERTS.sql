d8ae569f0304e51253dddc6e938c09694684ec5e
IanGandine <104468797+IanGandine@users.noreply.github.com>
2023-12-11 23:08:22 -0300

    Create INSERTS.sql

---
+++ INSERTS.sql
@@ -0,0 +1,75 @@
+USE sucos ;
+
+INSERT INTO tb_produto (
+produto,
+nome_produto,
+embalagem,
+preco_lista,
+tamanho,
+sabor) VALUES (
+'1040107',
+'Light - 350 ml - Melancia',
+'Lata',
+ 4.56,
+'350 ml',
+'Melancia');
+
+INSERT INTO tb_produto(
+produto,
+nome_produto,
+embalagem,
+preco_lista,
+tamanho,
+sabor) VALUES (
+'1037797',
+'Clean - 2 Litros - Laranja',
+'PET',
+ 16.01,
+'2000 ml',
+'Laranja');
+INSERT INTO tb_produto(
+produto,
+nome_produto,
+embalagem,
+preco_lista,
+tamanho,
+sabor) VALUES (
+'1000889',
+'Sabor da Montanha - 700 ml - Uva',
+'Garrafa',
+ 6.31,
+'700 ml',
+'Uva');
+
+INSERT INTO tb_produto(
+produto,
+nome_produto,
+embalagem,
+preco_lista,
+tamanho,
+sabor) VALUES (
+'1004327',
+'Videira do Campo - 1,5 Litros - Melância',
+'PET',
+ 19.51,
+'1,5 Litros',
+'Melância');
+
+INSERT INTO tb_produto (
+produto,
+nome_produto,
+embalagem,
+preco_lista,
+tamanho,
+sabor) VALUES
+('544931', 'Frescor do Verão - 350 ml - Limão', 'PET', 3.20,'350 ml','Limão');
+
+INSERT INTO tb_produto (
+produto,
+nome_produto,
+embalagem,
+preco_lista,
+tamanho,
+sabor) VALUES
+('1078680', 'Frescor do Verão - 470 ml - Manga', 'Lata',5.18, '470 ml','Manga');
+
